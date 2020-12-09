e7d2fe2c7919569352f48abd1f7a815c8157a7f8
noobdev <noobdev@dontmail.me>
2020-12-09 20:18:53 +0100

    Day 04 pt.1

---
+++ Day_04/part1.py
@@ -0,0 +1,22 @@
+separator = "\n\n"
+import re
+
+required_pattern="byr|iyr|eyr|hgt|hcl|ecl|pid"
+num_required_fields=7
+
+def run():
+    with open("input", "r") as input:
+        entries = input.read().split(separator)
+
+        total_valid_count=0
+
+        for e in entries:
+            matches = re.findall(required_pattern, e)
+            if len(matches) >= num_required_fields:
+                total_valid_count += 1
+
+        print(f"Result: {total_valid_count}")
+        
+
+if __name__ == "__main__":
+    run()
