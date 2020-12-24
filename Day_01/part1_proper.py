15aa7b5e97b90faeb1946e0bb95ee1dc660192b1
noobdev <noobdev@dontmail.me>
2020-12-24 19:43:58 +0100

    format

--- Day_01/part1_proper.py
+++ Day_01/part1_proper.py
@@ -1,23 +1,21 @@
-
 sum = 2020
+
 
 def run():
     with open("input", "r") as input:
-        numbers = input.read().split("\n")
+        numbers = input.read().splitlines()
 
     seenNumbers = dict()
 
     for numberStr in numbers:
         number = int(numberStr)
         diff = sum - int(number)
-        
+
         if diff in seenNumbers:
             print(f"Solution: {(diff * number)} ({diff} * {number} = {diff * number} | {diff} + {number} = {sum})")
             return
         else:
             seenNumbers[int(number)] = True
-    
-    
 
 
 if __name__ == "__main__":
